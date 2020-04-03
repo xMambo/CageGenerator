ae2322113734882c782c4aa4329db0865a658b97
xMambo <millerdesigncotn@gmail.com>
2020-04-03 11:59:24 -0500

    installed node-fetch, axios, and cheerio

---
+++ index.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+// this is the url for Nicolas Cage. if i wanted to change the actor i'd have to find a way to cut that into the url.
+const url = 'https://www.imdb.com/name/nm0000115/?ref_=nv_sr_srsg_4';
+
+function searchMovies() {
+    fetch(url)
+    .then(response => response.text())
+}
+
+searchMovies()
+.then(body => {
+    console.log(body);
+});
